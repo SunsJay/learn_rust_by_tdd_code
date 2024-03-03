312f607a2266be9759ac020eaf880da72d09d897
Suns Jay <sunsjay0806@gmail.com>
2024-03-03 09:41:38 +0800

    feat: Add linked_list

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -12,6 +12,12 @@ impl<T> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList { head: None }
     }
+
+    pub fn new_with_head_node(data: T) -> Self {
+        LinkedList {
+            head: Some(Box::new(Node { data, next: None })),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -20,8 +26,19 @@ mod tests {
     use crate::LinkedList;
     #[test]
     fn test_linked_list_creation() {
-        let mut list: LinkedList<i32> = LinkedList::new();
+        let list: LinkedList<i32> = LinkedList::new();
         assert_eq!(list.head, None);
     }
-}
 
+    #[test]
+    fn test_linked_list_creation_with_head_node() {
+        let list: LinkedList<i32> = LinkedList::new_with_head_node(10);
+        assert_eq!(
+            list.head,
+            Some(Box::new(Node {
+                data: 10,
+                next: None
+            }))
+        );
+    }
+}
