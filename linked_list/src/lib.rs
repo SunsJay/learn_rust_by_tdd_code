5f4ffdff7373c4bc8918a36714fb2569460e2b85
Suns Jay <sunsjay0806@gmail.com>
2024-03-05 18:33:35 +0800

    feat: add insert function

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 #[derive(Debug, PartialEq)]
 pub struct Node<T> {
     data: T,
@@ -17,6 +18,48 @@ impl<T> LinkedList<T> {
         LinkedList {
             head: Some(Box::new(Node { data, next: None })),
         }
+    }
+    // 创建一个新的结点，将新结点的 next 指向当前链表的 head 结点，然后将链表的 head 指向新结点
+    pub fn insert_head(&mut self, data: T) {
+        let new_node = Node {
+            data,
+            next: self.head.take(),
+        };
+        self.head = Some(Box::new(new_node));
+    }
+
+    pub fn insert_head_with_head_node(&mut self, data: T) {
+
+        let new_node = Node {
+            data,
+            next: self.head.as_mut().unwrap().next.take(),
+        };
+        
+        self.head.as_mut().unwrap().next = Some(Box::new(new_node));
+
+    }
+    // 遍历链表，直到找到最后一个结点，然后将新结点的 next 指向 None，将最后一个结点的 next 指向新结点
+    pub fn insert_tail(&mut self, data: T) {
+        let mut current = &mut self.head;
+        while let Some(node) = current {
+            current = &mut node.next;
+        }
+        *current = Some(Box::new(Node { data, next: None }));
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for LinkedList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "head -> ")?;
+        let mut current = &self.head;
+
+        while let Some(node) = current {
+            write!(f, "[ {} | * ] -> ", node.data)?;
+            current = &node.next;
+        }
+
+        write!(f, "[ None ]")?;
+        Ok(())
     }
 }
 
@@ -40,5 +83,7 @@ mod tests {
                 next: None
             }))
         );
+
+        println!("{}", list);
     }
 }
